0382444be455fc85d80805af8ac5c5603f5d4bfd
ikramMc <ki_mechitoua@esi.dz>
2023-01-10 14:18:38 +0100

    j'ai ajouter la page map component

---
+++ frontend/src/components/map.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+import MapPicker from 'react-google-map-picker'
+
+const DefaultLocation = { lat: 36.7, lng: 2.985};
+const DefaultZoom = 10;
+
+const App = () => {
+
+  const [defaultLocation, setDefaultLocation] = useState(DefaultLocation);
+
+  const [location, setLocation] = useState(defaultLocation);
+  const [zoom, setZoom] = useState(DefaultZoom);
+
+  function handleChangeLocation (lat, lng){
+    setLocation({lat:lat, lng:lng});
+  }
+  
+  function handleChangeZoom (newZoom){
+    setZoom(newZoom);
+  }
+
+  function handleResetLocation(){
+    setDefaultLocation({ ...DefaultLocation});
+    setZoom(DefaultZoom);
+  }
+
+  return (
+    <div>
+  <button onClick={handleResetLocation}>Reset Location</button>
+  <label>Latitute:</label><input type='text' value={location.lat} disabled/>
+  <label>Longitute:</label><input type='text' value={location.lng} disabled/>
+  <label>Zoom:</label><input type='text' value={zoom} disabled/>
+  
+  <MapPicker defaultLocation={defaultLocation}
+    zoom={zoom}
+    mapTypeId="roadmap"
+    style={{height:'700px'}}
+    onChangeLocation={handleChangeLocation} 
+    onChangeZoom={handleChangeZoom}
+    apiKey='AIzaSyD55lI3l65Vy6vvRL-sylk3hjJWy83iO3s'/>
+  </div>
+  );
+}
+
+export default App;
+//AIzaSyAv5mn7wj5dgxkjq875dLkX6YU9N76gpfw:not working
